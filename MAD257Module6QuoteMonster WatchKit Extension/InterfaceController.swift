d689b0b4427b834ab5d362f0f727e8053e87054a
km <35552602+catnap7777@users.noreply.github.com>
2021-02-23 18:12:30 -0600

    quotes added

--- MAD257Module6QuoteMonster WatchKit Extension/InterfaceController.swift
+++ MAD257Module6QuoteMonster WatchKit Extension/InterfaceController.swift
@@ -14,14 +14,20 @@ class InterfaceController: WKInterfaceController {
 
     @IBOutlet var quoteLabel: WKInterfaceLabel!
     @IBOutlet var hitMeButton: WKInterfaceButton!
-    @IBOutlet var quoteOutputText: WKInterfaceTextField!
+    @IBOutlet var quoteOutputLabel: WKInterfaceLabel!
     
     //var quoteArray = [(author: String, name: String)]()
     var quoteArray: [(author: String, quote: String)] =
         [("Albert Einstein", "I have no special talent. I am only passionately curious. "),
         ("Mother Teresa", "If you judge people, you have no time to love them."),
         ("Buddha", "All that we are is the result of what we have thought."),
-        ("Coco Chanel", "The most courageous act is still to think for yourself. Aloud.")
+        ("Coco Chanel", "The most courageous act is still to think for yourself. Aloud."),
+        ("Dalai Lama","Be kind whenever possible. It is always possible."),
+        ("LeBron James","I like criticism. It makes you strong."),
+        ("Benjamin Franklin","Well done is better than well said."),
+        ("Eleanor Roosevelt","The future belongs to those who believe in the beauty of their dreams."),
+        ("Michael Jordan","I’ve failed over and over and over again in my life and that is why I succeed."),
+        ("Oscar Wilde","Be yourself; everyone else is already taken.")
         ]
     
     override func awake(withContext context: Any?) {
@@ -33,8 +39,8 @@ class InterfaceController: WKInterfaceController {
     @IBAction func hitMeButtonPressed() {
        quoteLabel.setText("Getting a quote...")
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            let randomInt = Int.random(in: 0...3)
-            self.quoteOutputText.setText(self.quoteArray[randomInt].quote)
+            let randomInt = Int.random(in: 0...9)
+            self.quoteOutputLabel.setText(self.quoteArray[randomInt].quote)
             self.quoteLabel.setText(self.quoteArray[randomInt].author)
         }
         
